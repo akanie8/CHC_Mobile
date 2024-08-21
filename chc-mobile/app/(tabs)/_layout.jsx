7fb2fbce4acb756f379446a2200c5e45a36c75ff
akanie8 <docsean2001@gmail.com>
2024-08-21 22:41:39 +0200

    Added TabLayout

--- chc-mobile/app/(tabs)/_layout.jsx
+++ chc-mobile/app/(tabs)/_layout.jsx
@@ -1,11 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import {Tabs} from 'expo-router'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
 const TabLayout = () => {
   return (
-    <View>
-      <Text>nothing</Text>
-    </View>
+   <Tabs screenOptions={{headerShown: false}}>
+      <Tabs.Screen name='home' 
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color}) => <FontAwesome name="home" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen name='explore' 
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({color}) => <MaterialIcons name="explore" size={24} color={color} />
+        }}
+      />
+      <Tabs.Screen name='profile' 
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({color}) => <AntDesign name="profile" size={24} color={color} />
+        }}
+      />
+     
+   </Tabs>
   )
 }
 
--- chc-mobile/app/index.tsx
+++ chc-mobile/app/index.tsx
@@ -1,16 +1,8 @@
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        backgroundColor: 'yellow',
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <Redirect href={'/home'} options={{
+    
+  }} />
 }
