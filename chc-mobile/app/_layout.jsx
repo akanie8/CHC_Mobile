01ecf213f68a84a89590e5cbdf3a41e347e9a418
akanie8 <docsean2001@gmail.com>
2024-08-23 16:01:30 +0200

    Added Caching for existing users

--- chc-mobile/app/_layout.jsx
+++ chc-mobile/app/_layout.jsx
@@ -1,8 +1,26 @@
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import LoginScreen from "./../components/LoginScreen"
+import LoginScreen from "./../components/LoginScreen";
+import * as SecureStore from "expo-secure-store";
 export default function RootLayout() {
+
+  const tokenCache = {
+    async getToken(key){
+      try {
+        return SecureStore.getItemAsync(key);
+      } catch(err){
+        return null;
+      }
+    },
+    async saveToken(key, value){
+      try{
+        return SecureStore.setItemAsync(key, value);
+      } catch(err){
+        return
+      }
+    }
+  }
 
   useFonts({
     'DMSerifText': require('./../assets/fonts/DMSerifText-Italic.ttf'),
@@ -10,7 +28,7 @@ export default function RootLayout() {
     'SpaceMono': require('./../assets/fonts/SpaceMono-Regular.ttf')
   })
   return (
-    <ClerkProvider publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
 
     <SignedIn>
     <Stack screenOptions={{
