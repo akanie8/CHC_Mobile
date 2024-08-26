51d7a9af593e22df43794a67967351de4e1b8cc1
akanie8 <docsean2001@gmail.com>
2024-08-26 16:56:23 +0200

    removed

--- chc-mobile/configs/FirebaseConfig.jsx
+++ chc-mobile/configs/FirebaseConfig.jsx
@@ -7,13 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBeO-XMdWS8FERzo8YD2pd4DmYl1LChdEQ",
-  authDomain: "chc-mobile-9fb7a.firebaseapp.com",
-  projectId: "chc-mobile-9fb7a",
-  storageBucket: "chc-mobile-9fb7a.appspot.com",
-  messagingSenderId: "84575738790",
-  appId: "1:84575738790:web:e5ec5f1b0c80ae3af74d67",
-  measurementId: "G-WNCEJRNQ6F"
+
 };
 
 // Initialize Firebase
